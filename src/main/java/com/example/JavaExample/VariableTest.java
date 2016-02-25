8fc0cad38e13f484137e724678b37b02ebe05582
AMI <aremi84@gmail.com>
2016-02-26 01:27:17 +0900

    변수

---
+++ src/main/java/com/example/JavaExample/VariableTest.java
@@ -0,0 +1,25 @@
+package com.example.JavaExample;
+
+public class VariableTest {
+	
+	public static void main(String[] args) {
+//		char ch = 'A';
+//		int code = ch;
+//		
+//		int code = 65;
+//		char ch = (char) code;
+//		
+//		System.out.println(ch);
+//		System.out.println(code);
+		
+		byte b = 0;
+		int i = 0;
+		
+		for(int x=0; x<=270; x++) {
+			System.out.println(b++ +" : "+i++);
+		}
+		
+		
+	}
+	
+}
